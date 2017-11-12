0d63303fe2c405a875984a930e55dc147c6b1594
lakshmanpalitha <lakshmanpalitha@gmail.com>
2017-11-12 12:07:28 +0530

    integrate user profile edit page

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,7 +1,53 @@
+<?php include "common/head.php" ?>
+<?php include "common/nav.php" ?>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Microsola
- * Date: 11/6/17
- * Time: 12:10 AM
- */ 
+if(!$is_logged_in){
+    redirect('Home/Login');
+}
+
+if($this->session->flashdata('welcome') ){
+    echo "<h3>".$this->session->flashdata('welcome')."</h3>";
+}
+?>
+
+
+
+    <h1>This is profile page - welcome
+        <?php
+        echo $this->session->userdata('first_name')." ";
+        echo $this->session->userdata('last_name');
+        ?>
+    </h1>
+
+
+
+
+<?php echo form_open('Profile/editProfile'); ?>
+    <div class="container">
+        <h2>Login page</h2>
+        <?php if($this->session->flashdata('msg') ){
+            echo "<h3>".$this->session->flashdata('msg')."</h3>";
+        }
+        ?>
+        <div class="form-group">
+            <label for="first_name">First Name</label>
+            <input type="text" class="form-control" name="first_name" id="first_name" aria-describedby="emailHelp" placeholder="First Name">
+        </div>
+        <div class="form-group">
+            <label for="last_name">Last Name</label>
+            <input type="text" class="form-control" name="last_name" id="last_name" aria-describedby="emailHelp" placeholder="Last Name">
+        </div>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Email">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Edit my Profile</button>
+    </div>
+<?php echo form_close(); ?>
+
+
+
+<?php include "common/scripts.php" ?>
+<?php include "common/foot.php" ?>
